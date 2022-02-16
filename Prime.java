2a96727911aa6ee321d5bdbec4533695e822b9f1
Owais siddiqui <83368515+owaiss259@users.noreply.github.com>
2022-02-16 14:18:49 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -2,14 +2,15 @@ public class Prime
 {  
 public static void main(String args[])   
 {  
-int number=100;  
-System.out.print("List of even numbers from 1 to "+number+": ");  
-for (int i=1; i<=number; i++)   
-{  
-if (i%2==0)   
-{  
-System.out.print(i + " ");  
-}  
-}  
-}  
-}  
+int a=100;
+  if(i%2==0)
+  {
+    system.out.println("even");
+  }
+  else
+  {
+    system.out.println("odd");
+  }
+}
+}
+
